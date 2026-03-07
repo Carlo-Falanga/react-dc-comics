7d3170c88ecb8b1ff775f376bd65e7aac544c039
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-07 12:33:27 +0100

    add first part of the footer and style

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,162 @@
-export default function AppFooter(){
+import LogoFooter from "../assets/img/dc-logo-bg.png";
 
-    return(
-        <footer>Footer</footer>
-    )
+export default function AppFooter() {
+  return (
+    <footer className="bkg_img">
+      <div className="container overflow-hidden">
+        <div className="row text-white py-5 ">
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <h3 className="text-uppercase pb-2">Dc Comics</h3>
+                <ul className="p-0">
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                </ul>
+                <h3 className="text-uppercase pb-2">Shop</h3>
+                <ul className="p-0">
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Shop DC
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Shop DC Collectibles
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col">
+                <h3 className="text-uppercase pb-2">Dc</h3>
+                <ul className="p-0">
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col">
+                <h3 className="text-uppercase pb-2">Sites</h3>
+                <ul className="p-0">
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                  <li className="list-unstyled">
+                    <a href="" className="nav-link footer_text">
+                      Characters
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <img className="logo_footer" src={LogoFooter} alt="" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
