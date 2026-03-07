edc87f541b0d90f3d5c5918dff9938362b7c286c
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-07 10:49:05 +0100

    add main pending section and style

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,10 @@
 export default function AppMain() {
 
     return(
-        <main>Main</main>
+        <main>
+            <div className="container mt-3">
+                <div className="text-white fw-bolder py-5">Content goes here</div>
+            </div>
+        </main>
     )
 }
