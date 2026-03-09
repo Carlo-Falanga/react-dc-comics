6fc54e1df6d84e5bee4564676217a8c16833e96b
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-09 14:10:19 +0100

    fix icons and text centering

--- src/components/AppServices.jsx
+++ src/components/AppServices.jsx
@@ -8,25 +8,25 @@ export default function AppServices() {
   return (
     <section className="bg-primary">
       <div className="container">
-        <div className="row gy-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 py-5">
-          <div className="col d-flex align-items-center justify-content-center m-0">
+        <div className="row gy-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 pb-5">
+          <div className="col d-flex align-items-center justify-content-center">
             <img className="services_img" src={DigitalComicsImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Digital Comics</span>
           </div>
-          <div className="col d-flex align-items-center justify-content-center m-0">
+          <div className="col d-flex align-items-center justify-content-center">
             <img className="services_img" src={MerchandiseImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Dc Merchandise</span>
           </div>
 
-          <div className="col d-flex align-items-center justify-content-center m-0">
+          <div className="col d-flex align-items-center justify-content-center">
             <img className="services_img" src={SubscriptionImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Subscription</span>
           </div> 
-          <div className="col d-flex align-items-center justify-content-center m-0">
+          <div className="col d-flex align-items-center justify-content-center">
             <img className="services_img" src={ShopLocationImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Comic Shop Locator</span>
           </div>
-          <div className="col d-flex align-items-center justify-content-center m-0">
+          <div className="col d-flex align-items-center justify-content-center">
             <img className="services_img" src={VisaImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Dc Power Visa</span>
           </div>
