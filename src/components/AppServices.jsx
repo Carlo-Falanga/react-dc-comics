34f114b9255769ab712d9f673b10295019fe0422
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-09 14:06:23 +0100

    fix: remove excessive margin

--- src/components/AppServices.jsx
+++ src/components/AppServices.jsx
@@ -9,24 +9,24 @@ export default function AppServices() {
     <section className="bg-primary">
       <div className="container">
         <div className="row gy-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 py-5">
-          <div className="col d-flex align-items-center justify-content-center">
+          <div className="col d-flex align-items-center justify-content-center m-0">
             <img className="services_img" src={DigitalComicsImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Digital Comics</span>
           </div>
-          <div className="col d-flex align-items-center justify-content-center">
+          <div className="col d-flex align-items-center justify-content-center m-0">
             <img className="services_img" src={MerchandiseImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Dc Merchandise</span>
           </div>
 
-          <div className="col d-flex align-items-center justify-content-center">
+          <div className="col d-flex align-items-center justify-content-center m-0">
             <img className="services_img" src={SubscriptionImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Subscription</span>
           </div> 
-          <div className="col d-flex align-items-center justify-content-center">
+          <div className="col d-flex align-items-center justify-content-center m-0">
             <img className="services_img" src={ShopLocationImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Comic Shop Locator</span>
           </div>
-          <div className="col d-flex align-items-center justify-content-center">
+          <div className="col d-flex align-items-center justify-content-center m-0">
             <img className="services_img" src={VisaImg} alt="" />
             <span className="text-white ps-3 text-uppercase services_text">Dc Power Visa</span>
           </div>
